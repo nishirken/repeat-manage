3fe9d7e6c963df410cc28e549683d8593683773f
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-01-17 21:47:29 +0300

    test

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
 import Html exposing (..)
+import Html.Events exposing (..)
 import Browser
 
 type alias Model =
@@ -6,30 +7,40 @@ type alias Model =
   , symbols : List String
   }
 
-type alias Msg =
-  ChangeSymbol String
+type Msg
+  = ChangeSymbol String
   | AddSymbol String
   | RemoveSymbol String
 
 main = Browser.document
-  { init = () -> (Model "" [], Cmd.none)
-  , update = updatew
+  { init = init
+  , update = update
   , view = view
   , subscriptions = \_ -> Sub.none
   }
+
+init : () -> (Model, Cmd Msg)
+init _ = (Model "" [], Cmd.none)
+
+pop : List a -> List a
+pop xs = case (List.reverse >> List.tail) xs of
+  (Just t) -> List.reverse t
+  Nothing -> []
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     (ChangeSymbol symbol) -> ({ model | currentSymbol = symbol }, Cmd.none)
-    (AddSymbol symbol) -> ({ model | symbols = (List.append model.symbols [symbol]) })
-    (Remove symbol) -> ({ model | symbols = ((List.reverse >> List.tail >> List.reverse) model.symbols) })
+    (AddSymbol symbol) -> ({ model | symbols = (List.append model.symbols [symbol]) }, Cmd.none)
+    (RemoveSymbol symbol) ->
+      ({ model | symbols = pop model.symbols }, Cmd.none)
 
 view : Model -> Browser.Document Msg
 view model =
   { title = "Repeat"
   , body =
     [ div [] [text model.currentSymbol]
+    , div [] (map model.symbols (\s -> div [] [text s]))
     , button [onClick (AddSymbol "2323")] [text "Add"]
     , button [onClick (RemoveSymbol "2323")] [text "Remove"]
     ]
